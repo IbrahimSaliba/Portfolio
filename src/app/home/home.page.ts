aa081c1f9d44e410a2a8a5ae882dcb9003eac227
IbrahimSaliba <ibrahim.saliba18@gmail.com>
2022-10-24 12:23:33 +0300

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public progress = 0;
+  i =0;
+
+  constructor() {
+    
+  }
+  move() {
+    if (this.i == 0) {
+      this.i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 10;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 90) {
+          clearInterval(id);
+          this.i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+          elem.innerHTML = width  + "%";
+        }
+      }
+    }
+  }
+
+}
